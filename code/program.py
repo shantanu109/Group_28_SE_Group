dde2678153aefad50b53d9030c43a431afde070f
Sahil Nande <89487138+Sahil-Nande@users.noreply.github.com>
2021-09-01 21:44:11 -0400

    Update program.py

--- code/program.py
+++ code/program.py
@@ -1,1 +1,10 @@
-
+def number(n):
+        if n % 2 != 0:
+                return('Weird')
+        else:
+                if n >= 2 and n <= 5:
+                        return('Not Weird')
+                elif n >= 6 and n <= 20:
+                        return('Weird')
+                else:
+                        return('Not Weird')
