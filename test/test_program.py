d529be700fba5658b6b7bcfc79f195db64c22f33
Sahil Nande <89487138+Sahil-Nande@users.noreply.github.com>
2021-09-01 21:44:52 -0400

    Add files via upload

---
+++ test/test_program.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+sys.path.append('../code')
+
+import program
+
+class uTest(unittest.TestCase):
+        def test_run(self):
+                self.assertEqual(program.number(3), 'Weird')
+                self.assertEqual(program.number(24), 'Not Weird')
+                self.assertEqual(program.number(4), 'Not Weird')
+                self.assertEqual(program.number(18), 'Weird')
+                self.assertEqual(program.number(20), 'Weird')
+
+if __name__ == '__main__':
+        unittest.main()
